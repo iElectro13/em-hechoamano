e4b75af8ec3755454009401133cdf33e97f862fb
iElectro <ejose.acc@gmail.com>
2022-03-03 22:09:56 -0500

    Init config

---
+++ src/components/buttons/Logo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <div className='flex flex-col justify-center items-center'>
+            <h1 className='font-lora text-5xl text-gray-100'>E&M</h1>
+            <span className='font-handlee text-gray-100 text-lg'>Hecho a mano</span>
+        </div>
+    )
+}
+
+export default Logo
---
+++ src/components/containers/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../buttons/Logo'
+
+const Navbar = () => {
+  return (
+    <nav className='w-full bg-orange-400 py-1'>
+        <Logo />
+        
+    </nav>
+  )
+}
+
+export default Navbar
